5509d0ec8878425ae57ceedf406f33988cf255db
Pablo L Basanta <basanta79@gmail.com>
2018-11-16 06:29:23 +0100

    commit intermedio

--- level.js
+++ level.js
@@ -1,8 +1,13 @@
+const ACTORS = {
+    'o' : Coin
+}
+
 function Level(plan){
     this.width = plan[0].length;
     this.heigth = plan.length;
 
     this.grid = [];
+    this.actors = [];
 
     for (let y=0; y<this.heigth; y++){
         let line = plan[y];
@@ -10,6 +15,9 @@ function Level(plan){
         for (let x=0; x<this.width; x++){
             let character = line[x];
             let characterType = null;
+
+            let Actor = ACTORS[character];
+            if (Actor) this.push(new Actor(new Vector(x,y), character))
             if (character==='x') characterType= 'wall';
             else if (character==='!') characterType= 'lava';
 
